cc51b6b2eb90c32c61d09fe88546cd10d3fe4057
wangchengtao <915129420@qq.com>
2025-07-31 19:24:51 +0800

    交易微服务

--- hm-api/src/main/java/com/hmall/api/client/ItemClient.java
+++ hm-api/src/main/java/com/hmall/api/client/ItemClient.java
@@ -5,6 +5,7 @@ import com.hmall.api.dto.ItemDTO;
 import com.hmall.api.dto.OrderDetailDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Collection;
@@ -16,5 +17,6 @@ public interface ItemClient {
     @GetMapping("/items")
     List<ItemDTO> queryItemByIds(@RequestParam("ids") Collection<Long> ids);
 
-    void deductStock(List<OrderDetailDTO> detailDTOS);
+    @PutMapping("/stock/deduct")
+    void deductStock(Collection<OrderDetailDTO> items);
 }
